083b6b51f9d8fc055165d1526b8feec58b275fe3
rajkumar532 <82566390+rajkumar532@users.noreply.github.com>
2021-05-24 09:54:29 +0530

    Add files via upload

---
+++ Exp 1.2 Indentation.py
@@ -0,0 +1,20 @@
+  
+if 1:
+    print('true')
+     print('hi')
+else :
+    print('false')
+
+ #output: unexpected indent
+
+ #to correct this:
+
+if 1:
+    print('true')
+    print('hi')
+else :
+    print('false')
+
+ #output:
+  true
+   hi
